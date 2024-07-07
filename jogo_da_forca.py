5b9855a36702eabadee13e04812153803bc74487
Jonathan Miranda <146142589+Jonthmiranda@users.noreply.github.com>
2024-07-07 12:35:16 -0300

    Update jogo_da_forca.py

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,80 +1,85 @@
-#modulos
+#libraries
 import random
 import array
 
-#variaveis
-palavras = ['dicionário', 'intrínseco', 'prescindir', 'presunçoso', 'diligência', 'corroborar', 'intempérie',
+#variables
+
+#the words are in Portuguese!
+word = ['dicionário', 'intrínseco', 'prescindir', 'presunçoso', 'diligência', 'corroborar', 'intempérie',
             'perspicaz', 'recíproco', 'concessão', 'impressão', 'escrúpulo', 'inerente', 'respeito', 'peculiar',
             'denegrir', 'sagaz', 'âmago', 'negro', 'termo', 'êxito', 'mexer', 'nobre', 'senso', 'afeto', 'algoz',
             'imprescindível', 'condescendente', 'idiossincrasia', 'característica', 'concupiscência', 'extraordinário']
-palav_esc = random.choice(palavras) #palavra aleatório do vetor palavras
-vetor_letra = [] #letras
-vetor_adiv = [] #adivinhando
-vetor_ltent = [] #letras tentadas
-tentativas = 6
-let_tent = 1
-#descobrindo quantas letras tem, gravando nos vetores
-count = 0
-for x in palav_esc:
-    count += 1
-    vetor_letra.insert(count, x)
-    vetor_adiv.insert(count, ' _ ')
 
-#iniciando game
-letras = str
-while vetor_letra != vetor_adiv:
-    #caso erre 6 vezes
-    if tentativas == 0:
-        print("Infelizmente você perdeu!! a palavra era:", palav_esc)
+word_cho = random.choice(word) #vector word random words
+vector_letter = [] #letter
+vector_cho = [] #guessing
+vector_att = [] #attempted lyrics
+attempts = 6 #how many attempts
+let_att = 1 #how many were tried
+
+##discovering how many letters there are, recording in the vectors
+count = 0
+for x in word_cho:
+    count += 1
+    vector_letter.insert(count, x)
+    vector_cho.insert(count, ' _ ')
+
+#starting game
+letters = str
+while vector_letter != vector_cho:
+    #if you make a mistake 6 times
+    if attempts == 0:
+        print("Unfortunately you lost!! the word was:", word_cho)
         break
        
-    print("Você tem ", tentativas, " tentativas\n")
-    #imprimindo as letras ja tentadas
-    for x in vetor_ltent:
+    print("You got ", attempts, " tries\n")
+    #printing the letters already tried
+    for x in vector_att:
         print(x, end="")
     print("\n")
        
-    #imprimindo as letras adivinhadas
-    for x in vetor_adiv:
+    #printing the guessed letters
+    for x in vector_cho:
         print(x, end="")
-    #tentando adivinhar
-    letras = str(input("\n\nDigite uma letra ou a palavra: \n"))
+    #trying to guess
+    letters = str(input("\n\nEnter a letter or word: \n"))
     print("\n")
    
-    #se acertar a digitando a palavras
-    if letras == palav_esc:
-        #pulando varias linhas
+    #if you get it right typing the words
+    if letters == word_cho:
+        #skipping several lines
         for x in range(10):
             print("\n")
-        print("Parabens!!, você venceu")
+        print("Congratulations!! you won")
         break
    
-    tem_letra = 0
+    has_lyric = 0
     countara = 0
-    #caso tenha a letra
-    for x in vetor_letra:
-        if letras == x:
-            vetor_adiv.pop(countara)
-            vetor_adiv.insert(countara, letras)
-            tem_letra = 1
+    #if you have the lyrics
+    for x in vector_letter:
+        if letters == x:
+            vector_cho.pop(countara)
+            vector_cho.insert(countara, letters)
+            has_lyric = 1
         countara += 1
-    #adicionando letra digitada no vetor
-    vetor_ltent.insert(let_tent, letras)
-    let_tent += 1
+        
+    #adding typed letter in vector
+    vector_att.insert(let_att, letters)
+    let_att += 1
    
-    #caso não tenha a letra
-    if tem_letra == 0:
-        tentativas -= 1
+    #if you don't have the lyrics
+    if has_lyric == 0:
+        attempts -= 1
      
            
-    #pulando varias linhas
+    #skipping several lines
     for x in range(10):
         print("\n")
        
-#caso acerte a palavras  
+#if you get the words right 
 else:
-    #pulando varias linhas
+    #skipping several lines
     for x in range(10):
         print("\n")
-    print("A palavra era", palav_esc)
-    print("Parabens!! você venceu")
+    print("The word was", word_cho)
+    print("Congratulations!! you won")
